27856dbdeed9a441b59f4226c77371818a8fe38f
kevincdyer <kevin.dyer.nj@gmail.com>
2021-09-15 22:09:17 -0700

    HW3 Update

--- Forecast_Submissions/Dyer_HW3.py/week3_assignment_code.py
+++ Forecast_Submissions/Dyer_HW3.py/week3_assignment_code.py
@@ -55,16 +55,17 @@ print(min(flow))
 print(max(flow))
 print(np.mean(flow))
 print(np.std(flow))
+print(day in month == 9)
 
 # Making and empty list that I will use to store
-# index values I'm interested in
+# For number of dates flow was greater than or equall to 94 in September
 ilist = []
 
 # Loop over the length of the flow list
 # and adding the index value to the ilist
 # if it meets some criteria that I specify
 for i in range(len(flow)):
-        if flow [i] > 600 and month[i] == 7:
+        if flow [i] >= 94 and month[i] == 9:
                 ilist.append(i)
 
 # see how many times the criteria was met by checking the length
@@ -82,3 +83,62 @@ ilist2 = [i for i in range(len(flow)) if flow[i] > 600 and month[i]==7]
 print(len(ilist2))
 
 
+# %%
+# For Flow under my rate after the year 2000
+ilist = []
+
+# Loop over the length of the flow list
+# and adding the index value to the ilist
+# if it meets some criteria that I specify
+for i in range(len(flow)):
+        if flow [i] >= 94 and month[i] == 9 and year[i] >= 2000:
+                ilist.append(i)
+
+# see how many times the criteria was met by checking the length
+# of the index list that was generated
+print(len(ilist))
+
+# Grabbing out the data that met the criteria
+# This  subset of data is just the elements identified
+# in the ilist
+subset = [flow[j] for j in ilist]
+
+# Alternatively I could have  written the for loop I used
+# above to  create ilist like this
+ilist3 = [i for i in range(len(flow)) if flow[i] > 600 and month[i]==7]
+print(len(ilist3))
+
+# %%
+# %%
+# For Flow under my rate after the year 2010
+ilist = []
+
+# Loop over the length of the flow list
+# and adding the index value to the ilist
+# if it meets some criteria that I specify
+for i in range(len(flow)):
+        if flow [i] >= 94 and month[i] == 9 and year[i] >= 2010:
+                ilist.append(i)
+
+# see how many times the criteria was met by checking the length
+# of the index list that was generated
+print(len(ilist))
+
+# Grabbing out the data that met the criteria
+# This  subset of data is just the elements identified
+# in the ilist
+subset = [flow[j] for j in ilist]
+
+# Alternatively I could have  written the for loop I used
+# above to  create ilist like this
+ilist4 = [i for i in range(len(flow)) if flow[i] > 600 and month[i]==7]
+print(len(ilist4))
+# %%
+type(day)
+# %%
+type(year)
+# %%
+type(flow)
+# %%
+type(month)
+# %%
